fb2b9ea674fac424d314ac9f6e6a34b2d58fc115
Luis <luis_gc@zohomail.com>
2024-04-30 19:18:04 -0600

    Se actualizo codigo

---
+++ src/Convertir.java
@@ -0,0 +1,5 @@
+public class Convertir {
+    private int opcin;
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
-        System.out.println("Prueba");
-        System.out.println("Prueba");
-        System.out.println("Prueba");
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("-----------------------------)");
+            System.out.println("1)");
+            System.out.println("2)");
+            System.out.println("3)");
+            System.out.println("4)");
+            System.out.println("5)");
+            System.out.println("6)");
+            System.out.println("7) Salir)");
+            System.out.print("Elija opción: ");
+            opcion = scanner.nextInt();
+
+            if (opcion == 1){
+                System.out.println("Elija opción: ");
+
+            }else if (opcion == 2) {
+                System.out.println("Elija opción: ");
+
+            } else if (opcion == 3) {
+
+            } else if (opcion == 4) {
+
+            } else if (opcion == 5) {
+
+            } else if (opcion == 6) {
+
+            }else{
+                System.out.println("Opción incorrecta, vuelva a intentar...");
+            }
+
+        }while(opcion != 7);
+        scanner.close();
     }
 }
